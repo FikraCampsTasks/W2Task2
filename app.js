42b77ecc8b9efe700f7f4abfc58a7d9d9296a317
Salah Saleh <mr.salah1989@gmail.com>
2018-11-20 10:55:21 +0300

    init solution

--- app.js
+++ app.js
@@ -1,36 +1,53 @@
+// to emulate a full ES2015+ environment 
 import 'babel-polyfill'
+
+// Declare news globally
 let news;
 
-
+// Listener trigger on page complete loading
 document.addEventListener('DOMContentLoaded', ()=>{
+  // Assign value to global news variable to hold container of article
   news = document.getElementById('news');
+  // Declare and assign search to hold search input
   let search = document.getElementById('search')
-
+  // Listener Trigger when press any key in keyboard
   search.addEventListener('keyup', (event)=>{
-    // console.log(event)/
+    // if "Enter" key press the getNews function will call
     if(event.key == 'Enter') {
       getNews(search.value)
     }
   })
-
+  // Run one time with default value
   getNews('iraq')
 })
 
+/**
+  * Get news from "newsapi.org" and update UI.
+  * @param {string} query name of what you search for
+**/
 async function getNews(query) {
   let response = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=978d6c3818ff431b8c210ae86550fb1f`)
   let content = await response.json()
-  console.log(content)
   updateUI(content.articles.map(createArticle).join('\n'))
 }
 
+/**
+  * Update content of news container element
+  * @param {string} content
+**/
 function updateUI(content) {
   news.innerHTML = content
 }
 
+/**
+  * Create HTML representation of article.
+  * @param {object} article object holds data of article
+  * @param {number} i index of article
+  * @returns {string} html design of article
+**/
 function createArticle(article, i) {
   article.counter = 1
   return `
-
     <article id="${i}">
       <img width="124px" height="124px" src="${article.urlToImage}" alt="">
       <div id="text">
