61d4ef8ec77c1db75a9cbdf2e42404ce8909b5a8
Salah Saleh <mr.salah1989@gmail.com>
2018-11-20 14:24:47 +0300

    1stSolution

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-// to emulate a full ES2015+ environment 
-import 'babel-polyfill'
+// to emulate a full ES2015+ environment
+import "babel-polyfill";
+
 // Declare news globally
 let news;
 
 // Listener trigger on page complete loading
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener("DOMContentLoaded", () => {
   // Assign value to global news variable to hold container of article
-  news = document.getElementById('news');
+  news = document.getElementById("news");
   // Declare and assign search to hold search input
-  let search = document.getElementById('search')
+  let search = document.getElementById("search");
   // Listener Trigger when press any key in keyboard
-  search.addEventListener('keyup', (event)=>{
+  search.addEventListener("keyup", event => {
     // if "Enter" key press the getNews function will call
-    if(event.key == 'Enter') {
-      getNews(search.value)
+    if (event.key == "Enter") {
+      getNews(search.value);
     }
-  })
+  });
   // Run one time with default value
-  getNews('iraq')
-})
+  getNews("iraq");
+});
 
 /**
-  * Get news from "newsapi.org" and update UI.
-  * @param {string} query name of what you search for
-**/
+ * Get news from "newsapi.org" and update UI.
+ * @param {string} query name of what you search for
+ **/
 async function getNews(query) {
-  let response = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=978d6c3818ff431b8c210ae86550fb1f`)
-  let content = await response.json()
-  updateUI(content.articles.map(createArticle).join('\n'))
+  let response = await fetch(
+    `https://newsapi.org/v2/everything?q=${query}&apiKey=978d6c3818ff431b8c210ae86550fb1f`
+  );
+  let content = await response.json();
+  updateUI(content.articles.map(createArticle).join("\n"));
 }
 
 /**
-  * Update content of news container element
-  * @param {string} content
-**/
+ * Update content of news container element
+ * @param {string} content
+ **/
 function updateUI(content) {
-  news.innerHTML = content
+  news.innerHTML = content;
 }
 
 /**
-  * Create HTML representation of article.
-  * @param {object} article object holds data of article
-  * @param {number} i index of article
-  * @returns {string} html design of article
-**/
+ * Create HTML representation of article.
+ * @param {object} article object holds data of article
+ * @param {number} i index of article
+ * @returns {string} html design of article
+ **/
 function createArticle(article, i) {
-  article.counter = 1
+  // Using url as id to retrieve counter value
+  article.counter = retrieveVote(article.url);
   return `
     <article id="${i}">
       <img width="124px" height="124px" src="${article.urlToImage}" alt="">
@@ -55,10 +59,57 @@ function createArticle(article, i) {
         <time>${article.publishedAt}</time>
       </div>
       <div id="voter">
-        <img height="13px" src="${require('./assets/upvote.svg')}" alt="">
-        <div id="counter${i}">${article.counter}</div>
-        <img  height="13px" src="${require('./assets/downvote.svg')}" alt="">
+        <img height="13px" onclick="incVote('counter${i}');" src="${require("./assets/upvote.svg")}" alt="">
+        <div id="counter${i}" url=${article.url}>${article.counter}</div>
+        <img  height="13px" onclick="decVote('counter${i}');" src="${require("./assets/downvote.svg")}" alt="">
       </div>
     </article>
-  `
+  `;
+}
+
+/**
+ * Store vote counter to countersDB in local storage.
+ * @param {String} id used as key in countersDB
+ * @param {number} count number of votes
+ **/
+function storeVote(id, count) {
+  let countersDB = JSON.parse(localStorage.getItem("countersDB") || "{}");
+  countersDB[id] = count;
+  localStorage.setItem("countersDB", JSON.stringify(countersDB));
+}
+
+/**
+ * Retrieve vote counter from countersDB in local storage.
+ * @param {String} id used as key in countersDB
+ * @returns {string} number of votes
+ **/
+function retrieveVote(id) {
+  let countersDB = JSON.parse(localStorage.getItem("countersDB") || "{}");
+  return countersDB[id] || 0;
+}
+
+// use 'window.' to declare global function
+
+// Increment vote counter by one
+window.incVote = function(id) {
+  updateCounter(id, 1);
+};
+
+// Decrement vote counter by one
+window.decVote = function(id) {
+  updateCounter(id, -1);
+};
+
+/**
+ * Update counter value in html and store it to localstorage.
+ * @param {String} id used as key in countersDB
+ * @param {String} number value to add to counter
+ **/
+function updateCounter(id, number) {
+  let counter = document.getElementById(id);
+  counter.innerText = Number(counter.innerText) + number;
+  // use url as id
+  let storeId = counter.getAttribute("url");
+  let count = counter.innerText;
+  storeVote(storeId, count);
 }
